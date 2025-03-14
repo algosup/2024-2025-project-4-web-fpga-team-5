8f4fd8b2420035e74aac007935fc05ae2fc4af16
Mathias "Mistz" Gagnepain <mathias.gagnepain@algosup.com>
2025-03-14 12:01:18 +0100

    fixing the preview host in vite.config to be able to do tests using render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['two024-2025-project-4-web-fpga-team-5.onrender.com']
+  }
 })
